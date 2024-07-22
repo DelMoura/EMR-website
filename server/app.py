9886d2c9f84161187663c3fc7d9f2e24f1bb47f5
DelMoura <jeferson.santos2566@gmail.com>
2024-07-21 22:36:40 -0300

    Maked: persing data in mongo

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_mongoengine import MongoEngine
 from dotenv import load_dotenv
 import os
@@ -8,6 +8,26 @@ load_dotenv()
 
 app = Flask(__name__)
 api = Api(app)
+
+UserModel_ps = reqparse.RequestParser()
+
+UserModel_ps.add_argument('email',
+                          type=str,
+                          required=True,
+                          help="Ocorreu um erro no email"
+                          )
+
+UserModel_ps.add_argument('name',
+                          type=str,
+                          required=True,
+                          help="Ocorreu um erro no nome",
+                          )
+
+UserModel_ps.add_argument('text',
+                          type=str,
+                          required=True,
+                          help="Ocorreu um erro no texto",
+                          )
 
 app.config['MONGODB_SETTINGS'] = {
     "db": os.getenv("DB_NAME"),
@@ -33,9 +53,16 @@ class User(Resource):
     def get(self):
         return jsonify(UserModel.objects())
 
+class UserPost(Resource):
+    def post(self):
+        data = UserModel_ps.parse_args()
+        UserModel(**data).save()
+
+        
 
 api.add_resource(HelloWorld, '/')
-api.add_resource(User, '/users')
+api.add_resource(User, '/user')
+api.add_resource(UserPost, '/users')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
